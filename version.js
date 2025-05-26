eede8931ad077974ce6ee7384188d01cda338d2c
Jottynha <Jottynha@users.noreply.github.com>
2025-05-26 23:16:47 +0000

    deploy: c8a616837139282c4ff82b229ce1031cb4129a4b

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION = { msg: `calendario-home`, hash: '13c8ec5', date: '2025-05-26 20:08:49' };
+window.APP_VERSION = { msg: `calendario-update`, hash: 'c8a6168', date: '2025-05-26 20:16:30' };
