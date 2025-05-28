b893b42889ac5b7a799566276f479eb8c8c433f0
PedroAugusto08 <PedroAugusto08@users.noreply.github.com>
2025-05-28 00:43:16 +0000

    deploy: 17acacf17ae9554b44746a527f17b2f331e8892c

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION = { msg: `Melhoria no calendario`, hash: 'ab1f8a8', date: '2025-05-27 11:41:21' };
+window.APP_VERSION = { msg: `Sprites de itens e modificações da loja/inventario`, hash: '17acacf', date: '2025-05-27 21:42:37' };
