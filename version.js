a0142ea149a0c17ac7b7b8166fecb86014af9b74
Ak4ai <Ak4ai@users.noreply.github.com>
2025-05-29 22:12:12 +0000

    deploy: ce0fbbcfe10e6528d3e6232a226e760a85af38a9

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION = { msg: `diasSemanatype + edit modal revisioned`, hash: '909e56f', date: '2025-05-29 18:53:53' };
+window.APP_VERSION = { msg: `85 teste`, hash: 'ce0fbbc', date: '2025-05-29 19:12:00' };
