de58e285f719853c817f31ef8e4f6a9f132cbb18
Ak4ai <Ak4ai@users.noreply.github.com>
2025-06-02 19:53:25 +0000

    deploy: 80640a516ae069b4b7f23d74a6c92642872161b7

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION = { msg: `teste notificação ios 91_4`, hash: 'f2590da', date: '2025-06-02 16:23:56' };
+window.APP_VERSION = { msg: `teste 91_5`, hash: '80640a5', date: '2025-06-02 16:53:13' };
