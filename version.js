6276093122e862ce427f1aacea9b99d10fa381e3
PedroAugusto08 <PedroAugusto08@users.noreply.github.com>
2025-06-04 18:17:03 +0000

    deploy: 032936a4a0b653ab2111bd978fd0eb3627220e89

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION = { msg: `Arrumando sistema de amizade`, hash: '6d159dd', date: '2025-06-04 14:47:44' };
+window.APP_VERSION = { msg: `Ajuste para aparecer os amigos na aba de amigos`, hash: '032936a', date: '2025-06-04 15:16:38' };
