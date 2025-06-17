e8995653ed179ea9c11e60787af0eb551e333790
PedroAugusto08 <PedroAugusto08@users.noreply.github.com>
2025-06-17 17:28:00 +0000

    deploy: b0bff84980c8b9f6fd774b84500b7c5f1ee01f92

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION = { msg: `Ajuste 96`, hash: '16f711c', date: '2025-06-14 11:30:30' };
+window.APP_VERSION = { msg: `Botão de ajuda por aba na top bar`, hash: 'b0bff84', date: '2025-06-17 14:27:41' };
