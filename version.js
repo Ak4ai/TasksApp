2b783de3d96723c0145b35cfb08e175f3ba87909
PedroAugusto08 <PedroAugusto08@users.noreply.github.com>
2025-06-25 21:08:26 +0000

    deploy: 4204ca4a6446c8a716997b09422d565606a95622

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION = { msg: `Correção de ajuste do IOS`, hash: 'ca97280', date: '2025-06-25 18:01:23' };
+window.APP_VERSION = { msg: `teste`, hash: '4204ca4', date: '2025-06-25 18:08:05' };
