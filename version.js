47d8774df6d77c13f8e913dba85800b0ee435947
PedroAugusto08 <PedroAugusto08@users.noreply.github.com>
2025-06-25 21:01:39 +0000

    deploy: ca972805596a11f43a3e4bc341919c2b040e2e85

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION = { msg: `Ajuste do botão de ajuda no IOS`, hash: '91a2e96', date: '2025-06-25 17:56:38' };
+window.APP_VERSION = { msg: `Correção de ajuste do IOS`, hash: 'ca97280', date: '2025-06-25 18:01:23' };
